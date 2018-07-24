24cd6d96218039befdddb88f6a28b5a90690a8f5
Kevin Klein <kevin.klein@gmail.com>
2018-07-23 21:14:57 -0700

    add id

--- themes/giorg/template-parts/views/acf-pagebuilder-content-row.php
+++ themes/giorg/template-parts/views/acf-pagebuilder-content-row.php
@@ -1,11 +1,13 @@
+<section id="<?php if($content_row_title): echo (str_replace(' ', '-', strtolower($content_row_title))); endif; ?>">
+
 <?php if($content_row_title): echo '<h3 class="header-full"><span>' . $content_row_title . '</span></h3>'; endif; ?>
 
-<section class="row-container <?php if($content_row_wrap_css_class): echo $content_row_wrap_css_class; endif;?>">
+    <div class="row-container <?php if($content_row_wrap_css_class): echo $content_row_wrap_css_class; endif;?>">
+        <div class="<?php if($content_row_wrap_inner_css_class): echo $content_row_wrap_inner_css_class; endif;?>">
 
-    <div class="<?php if($content_row_wrap_inner_css_class): echo $content_row_wrap_inner_css_class; endif;?>">
+            <?php if($content_row_html): echo $content_row_html; endif; ?>
 
-        <?php if($content_row_html): echo $content_row_html; endif; ?>
-
+        </div>
     </div>
 
 </section>
