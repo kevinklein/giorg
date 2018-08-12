98420e7a322e88ff36fd524bd38e0299a25c4ef8
Kevin Klein <kevin.klein@gmail.com>
2018-08-12 16:36:53 -0700

    updates

--- themes/giorg/partials/breadcrumbs.php
+++ themes/giorg/partials/breadcrumbs.php
@@ -1,11 +1,16 @@
 <?php if ( !get_field( 'hide_breadcrumbs' ) ): ?>
 	<nav class="breadcrumb p-y border-bottom">
-		<?php
-		if ( function_exists('yoast_breadcrumb') ) {
-		yoast_breadcrumb('
-		<div class="container"><svg class="icon icon-home2"><use xlink:href="#icon-home2"></use></svg>','</div>
-		');
-		}
-		?>
+		<?php if( is_page( 'contentapi' ) ){ ?>
+			<!-- BREADCRUMBBEGIN -->
+			<!-- BREADCRUMBEND -->
+        <?php } else { ?>
+			<?php
+			if ( function_exists('yoast_breadcrumb') ) {
+			yoast_breadcrumb('
+			<div class="container"><svg class="icon icon-home2"><use xlink:href="#icon-home2"></use></svg>','</div>
+			');
+			}
+			?>
+		<?php } ?>
 	</nav>
 <?php endif; ?>
--- themes/giorg/partials/page-title.php
+++ themes/giorg/partials/page-title.php
@@ -1,7 +1,14 @@
 <?php if ( !get_field( 'hide_header' ) ): ?>
 	<header class="border-bottom">
 		<div class="container p-y-md"> 
-			<?php the_title( '<h1 class="main-title">', '</h1>' ); ?>      
+			<?php if( is_page( 'contentapi' ) ){ ?>
+				<h1 class="main-title">
+					<!-- TITLEBEGIN -->
+					<!-- TITLEEND -->
+				</h1>
+			<?php } else { ?>
+				<?php the_title( '<h1 class="main-title">', '</h1>' ); ?> 
+			<?php } ?>     
 		</div>
 	</header>
 <?php endif; ?>
