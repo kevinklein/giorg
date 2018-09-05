a97732c52ebbebeb8467c4e611f0bebbd4c95d63
Kevin Klein <kevin.klein@gmail.com>
2018-09-04 19:23:59 -0700

    patients template

---
+++ themes/giorg/page-patients.php
@@ -0,0 +1,23 @@
+
+<?php
+/**
+ * Template Name: Patients
+ *
+ * @package giorg
+ */
+
+get_header();
+?>
+
+	<?php
+	while ( have_posts() ) :
+		the_post();
+	
+		get_template_part( 'template-parts/content', 'page' );
+
+	endwhile; // End of the loop.
+	?>
+
+<?php
+get_footer(); 
+?>
