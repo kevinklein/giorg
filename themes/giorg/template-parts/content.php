35f8219dbf76ab63a82ac31fba8fc9448469d8f9
Kevin Klein <kevin.klein@gmail.com>
2018-11-24 21:08:12 -0800

    blog

--- themes/giorg/template-parts/content.php
+++ themes/giorg/template-parts/content.php
@@ -11,12 +11,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php $postThumb = get_the_post_thumbnail(); if( !empty( $postThumb ) ) : ?>
-	
-		<a href="<?php the_permalink(); ?>" class="post-thumbnail pull-left m-r m-b">
-			<?php the_post_thumbnail('medium'); ?> 
-		</a>
-
+	<?php if ( !is_singular() ) : ?>
+		<?php $postThumb = get_the_post_thumbnail(); if( !empty( $postThumb ) ) : ?>
+			<a href="<?php the_permalink(); ?>" class="post-thumbnail pull-left m-r m-b">
+				<?php the_post_thumbnail('medium'); ?> 
+			</a>
+		<?php endif; ?>
 	<?php endif; ?>
 
 	<header class="entry-header">
