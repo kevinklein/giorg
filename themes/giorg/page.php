2721f3cc6e721b82dcbd449ba20ea9bc2d7ce761
Kevin Klein <kevin.klein@gmail.com>
2019-01-25 11:46:37 -0800

    split content

--- themes/giorg/page.php
+++ themes/giorg/page.php
@@ -19,19 +19,21 @@ get_header();
 	while ( have_posts() ) :
 		the_post();
 
-		get_template_part( 'template-parts/content', 'page' );
-
 		if ( is_page( 'gi-fellowship-program-information') ) :
 
 			get_template_part( 'template-parts/content-fellowship', 'page' );
 
-		elseif ( is_page( 'guidelines') ) :
-
-			get_template_part( 'template-parts/content-guidelines', 'page' );
-
 		elseif ( is_page( 'history') ) :
 
 			get_template_part( 'template-parts/content-history', 'page' );
+
+		endif;
+	
+		get_template_part( 'template-parts/content', 'page' );
+
+		if ( is_page( 'guidelines') ) :
+
+			get_template_part( 'template-parts/content-guidelines', 'page' );
 
 		endif;
 
