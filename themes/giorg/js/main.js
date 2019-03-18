614cd400c0284912d83468c045bee04d891fbc21
Kevin Klein <kevin.klein@gmail.com>
2019-03-17 18:10:40 -0700

    more js

--- themes/giorg/js/main.js
+++ themes/giorg/js/main.js
@@ -161,12 +161,40 @@
             }
         });
         
-        $('.read-more a').click(function(e) {
-            $(this).parents('.read-more-parent').toggleClass('read-more-parent-expanded');
-            $(this).html() == 'Read Less' ? $(this).html('Read More') : $(this).html('Read Less');
-            e.preventDefault();
-        });
+        var showChar = 100;  // How many characters are shown by default
+        var ellipsestext = "...";
+        var moretext = "Show more >";
+        var lesstext = "Show less";
         
+
+        $('.more').each(function() {
+            var content = $(this).html();
+    
+            if(content.length > showChar) {
+    
+                var c = content.substr(0, showChar);
+                var h = content.substr(showChar, content.length - showChar);
+    
+                var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+    
+                $(this).html(html);
+            }
+    
+        });
+    
+        $(".morelink").click(function(){
+            if($(this).hasClass("less")) {
+                $(this).removeClass("less");
+                $(this).html(moretext);
+            } else {
+                $(this).addClass("less");
+                $(this).html(lesstext);
+            }
+            $(this).parent().prev().toggle();
+            $(this).prev().toggle();
+            return false;
+        });
+            
         $('[data-toggle="tooltip"], .tooltip-trigger').tooltip();
         
         $('.popover-dismiss').popover({
